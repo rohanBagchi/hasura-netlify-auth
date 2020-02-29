803de0fad8434cd8445280165f3dd2d49502caf2
Rohan Bagchi <rohan.bagchi01@gmail.com>
2020-02-29 11:15:57 +0100

    WIP: adding identity fn

---
+++ functions/identity-signup/identity-signup.js
@@ -0,0 +1,47 @@
+// note - this function MUST be named `identity-signup` to work
+// we do not yet offer local emulation of this functionality in Netlify Dev
+//
+// more:
+// https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
+// https://www.netlify.com/docs/functions/#identity-and-functions
+const axios = require("axios");
+
+exports.handler = async function(event, context) {
+  const data = JSON.parse(event.body)
+  const { user } = data
+
+  const responseBody = {
+    app_metadata: {
+      roles: user.email.split('@')[1] === 'trust-this-company.com' ? ['editor'] : ['visitor'],
+      my_user_info: 'this is some user info'
+    },
+    user_metadata: {
+      ...user.user_metadata, // append current user metadata
+      custom_data_from_function: 'hurray this is some extra metadata'
+    }
+  };
+
+  axios.post({
+    url: "https://hasura-app-with-netlify.herokuapp.com/v1/graphql",
+    headers: {
+      ["x-hasura-admin-secret"]: process.env.HASURA_SECRET
+    },
+    body: {
+      query: `
+        mutation addUser($id: String, $name: String, $email: String) {
+          insert_users(objects: {id: $id, email: $email, name: $name}) {
+            affected_rows
+          }
+        }
+      `,
+      variables: {
+        id: user.id
+      }
+    }
+  })
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody)
+  }
+}
